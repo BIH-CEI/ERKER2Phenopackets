6f24d08665ed214a5ad2f1c4a8c6ebf998f9c985
Filip Rehburg <fil.rehburg@gmail.com>
2023-08-24 16:01:22 +0200

    Reformatted map phenopackets 2 json file and added comment

--- Phenopackets2JSON.py
+++ Phenopackets2JSON.py
@@ -3,16 +3,21 @@ from typing import List, Dict
 
 from phenopackets import Phenopacket
 from google.protobuf.json_format import MessageToJson
-def map_phenopacket2json(phenopacket:Phenopacket) -> str:
+
+
+def map_phenopacket2json(phenopacket: Phenopacket) -> str:
     return MessageToJson(phenopacket)
+
 
 def write_phenopacket2json_file(phenopacket: Phenopacket, path: str) -> None:
     json_str = map_phenopacket2json(phenopacket)
     out_path = os.path.join(path, phenopacket.id)
     with open(out_path, 'w') as fh:
         fh.write(json_str)
+        print(f'Successfully wrote phenopacket to JSON {path}')
 
-def write_phenopackets2json_file(phenopackets_list: List[Phenopacket], path:str) -> None:
+
+def write_phenopackets2json_file(phenopackets_list: List[Phenopacket], path: str) -> None:
     # Make sure output path exists
     os.makedirs(path, exist_ok=True)
 
