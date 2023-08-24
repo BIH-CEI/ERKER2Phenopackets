cfe602359d6721f88c7a49586c59eeb042ae6bf4
Filip Rehburg <fil.rehburg@gmail.com>
2023-08-24 16:00:18 +0200

    Implemented map phenopacket 2 json

--- Phenopackets2JSON.py
+++ Phenopackets2JSON.py
@@ -6,14 +6,15 @@ from google.protobuf.json_format import MessageToJson
 def map_phenopacket2json(phenopacket:Phenopacket) -> str:
     return MessageToJson(phenopacket)
 
-def write_phenopacket2json_file(phenopacket: Phenopacket, path: str) ->
+def write_phenopacket2json_file(phenopacket: Phenopacket, path: str) -> None:
+    json_str = map_phenopacket2json(phenopacket)
+    out_path = os.path.join(path, phenopacket.id)
+    with open(out_path, 'w') as fh:
+        fh.write(json_str)
 
 def write_phenopackets2json_file(phenopackets_list: List[Phenopacket], path:str) -> None:
     # Make sure output path exists
     os.makedirs(path, exist_ok=True)
 
     for phenopacket in phenopackets_list:
-        json = map_phenopacket2json(phenopacket)
-        fpath = os.path.join(path, phenopacket)
-        with open(fpath, 'w') as fh:
-            fh.write(json)
+        write_phenopacket2json_file(phenopacket, path)
