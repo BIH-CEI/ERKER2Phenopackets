ef12b8f50311621306326909acf30d4e3abbde7d
Filip Rehburg <fil.rehburg@gmail.com>
2023-08-24 16:32:01 +0200

    Reformatted ERKER2Phenopackets file

--- MapERKER2Phenopackets.py
+++ MapERKER2Phenopackets.py
@@ -14,43 +14,34 @@ from google.protobuf.internal.well_known_types import Timestamp
 from ParseErker import parse_erker_date_of_birth, parse_erker_sex
 
 
-def create_resource(phenopacket_id: str, name: str, namespace_prefix: str, url: str, version: str,
-                    iri_prefix: str) -> Resource:
-    """A convenience method to create a Phenopacket Schema resource.
+def map_erker2phenopackets(df: pd.DataFrame, created_by: str):
+    erker_phenopackets = []
+    # vorher quasi nur mapping, hier zusammensetzen
+    resources = [
+        create_resource('ncbitaxon', 'NCBI organismal classification', 'NCBITaxon',
+                        'http://purl.obolibrary.org/obo/ncbitaxon.owl', '2021-06-10',
+                        'http://purl.obolibrary.org/obo/NCBITaxon_')
+    ]
 
-    :param phenopacket_id: The id of the resource.
-    :type phenopacket_id: str
-    :param name: The name of the resource.
-    :type name: str
-    :param namespace_prefix: The namespace prefix of the resource.
-    :type namespace_prefix: str
-    :param url: The url of the resource.
-    :type url: str
-    :param version: The version of the resource.
-    :type version: str
-    :param iri_prefix: The iri prefix of the resource.
-    :type iri_prefix: str
-    :return: A Phenopacket Schema Resource object.
-    :rtype: Resource
-    """
-    return Resource(
-        id=phenopacket_id, # The id of the resource is not the same as the id of the phenopacket.
-        name=name, namespace_prefix=namespace_prefix,
-        url=url, version=version, iri_prefix=iri_prefix
-    )
+    for i, (phenopacket_id, row) in enumerate(df.iterrows()):
+        phenopacket_id = f'{i}-{phenopacket_id}'  # TODO: can we come up with a better ID?
+        phenopacket = map_erker_row2phenopacket(phenopacket_id, row, resources, created_by)
+        erker_phenopackets.append(phenopacket)
+
+    print(f'Mapped {len(erker_phenopackets)} phenopackets')
+    return erker_phenopackets
 
 
 def map_erker_row2phenopacket(
         phenopacket_id: str, row: pd.Series,
         resources: List[Resource], created_by: str, phenopacket_schema_version=phenopackets.__version__
 ):
-    
     ## Subject
     # The elements we may consider adding:
     #  age at last encounter, vital status, karyotypic sex, gender
 
     subject = Individual(
-        id=phenopacket_id, # TODO: is this a valid id here?
+        id=phenopacket_id,  # TODO: is this a valid id here?
         date_of_birth=parse_erker_date_of_birth(row['sct_184099003_y']),
         sex=parse_erker_sex(row['sct_281053000']),
         taxonomy=OntologyClass(id='NCBITaxon:9606', label='Homo sapiens')
@@ -71,24 +62,24 @@ def map_erker_row2phenopacket(
 
     ## Interpretations
     interpretation = Interpretation(
-        id=phenopacket_id, # TODO: is this a valid id here?
+        id=phenopacket_id,  # TODO: is this a valid id here?
         progress_status='SOLVED',
         # diagnosis=Diagnosis(phenopacket_id='ORPHA:71529', label='Obesity due to melanocortin 4 receptor deficiency'),
     )
 
     # TODO - add variants
     variantInterpretation = VariantInterpretation(
-        acmg_pathogenicity_classification='NOT_PROVIDED',  # acmg will be added
+        acmg_pathogenicity_classification='NOT_PROVIDED',  # TODO: acmg will be added
         therapeutic_actionability='UNKNOWN_ACTIONABILITY',
         #  variant=parse_erker_hgvs()
 
-    )
+    )  # TODO: this is not used in the phenopacket definition below
 
     ## Disease
     disease = Disease(
         term=OntologyClass(id='ORPHA:71529', label='Obesity due to melanocortin 4 receptor deficiency'),
         #    onset=parse_erker_onset(row['sct_424850005']),
-    )
+    )  # TODO: this is not used in the phenopacket definition below
 
     ## MetaData
     created = Timestamp()
@@ -103,7 +94,7 @@ def map_erker_row2phenopacket(
     )
 
     phenopacket = Phenopacket(
-        id=phenopacket_id, # TODO: is this a valid id here?
+        id=phenopacket_id,  # TODO: is this a valid id here?
         subject=subject,
         phenotypic_features=[phenotypicFeatures],
         interpretations=[interpretation],
@@ -114,19 +105,27 @@ def map_erker_row2phenopacket(
     return phenopacket
 
 
-def map_erker2phenopackets(df: pd.DataFrame, created_by: str):
-    erker_phenopackets = []
-    # vorher quasi nur mapping, hier zusammensetzen
-    resources = [
-        create_resource('ncbitaxon', 'NCBI organismal classification', 'NCBITaxon',
-                        'http://purl.obolibrary.org/obo/ncbitaxon.owl', '2021-06-10',
-                        'http://purl.obolibrary.org/obo/NCBITaxon_')
-    ]
+def create_resource(phenopacket_id: str, name: str, namespace_prefix: str, url: str, version: str,
+                    iri_prefix: str) -> Resource:
+    """A convenience method to create a Phenopacket Schema resource.
 
-    for i, (phenopacket_id, row) in enumerate(df.iterrows()):
-        phenopacket_id = f'{i}-{phenopacket_id}' # TODO: can we come up with a better ID?
-        phenopacket = map_erker_row2phenopacket(phenopacket_id, row, resources, created_by)
-        erker_phenopackets.append(phenopacket)
-
-    print(f'Mapped {len(erker_phenopackets)} phenopackets')
-    return erker_phenopackets
+    :param phenopacket_id: The id of the resource.
+    :type phenopacket_id: str
+    :param name: The name of the resource.
+    :type name: str
+    :param namespace_prefix: The namespace prefix of the resource.
+    :type namespace_prefix: str
+    :param url: The url of the resource.
+    :type url: str
+    :param version: The version of the resource.
+    :type version: str
+    :param iri_prefix: The iri prefix of the resource.
+    :type iri_prefix: str
+    :return: A Phenopacket Schema Resource object.
+    :rtype: Resource
+    """
+    return Resource(
+        id=phenopacket_id,  # The id of the resource is not the same as the id of the phenopacket.
+        name=name, namespace_prefix=namespace_prefix,
+        url=url, version=version, iri_prefix=iri_prefix
+    )
