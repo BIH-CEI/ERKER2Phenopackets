93ad20f2940a9bd2d1a0e27b018abcbfc5b0379f
Filip Rehburg <fil.rehburg@gmail.com>
2023-08-24 16:07:13 +0200

    Renamed methods and added prints in Phenopackets2JSON.py

--- Phenopackets2JSON.py
+++ Phenopackets2JSON.py
@@ -5,21 +5,23 @@ from phenopackets import Phenopacket
 from google.protobuf.json_format import MessageToJson
 
 
-def map_phenopacket2json(phenopacket: Phenopacket) -> str:
+def _map_phenopacket2json_str(phenopacket: Phenopacket) -> str:
     return MessageToJson(phenopacket)
 
 
-def write_phenopacket2json_file(phenopacket: Phenopacket, path: str) -> None:
-    json_str = map_phenopacket2json(phenopacket)
-    out_path = os.path.join(path, phenopacket.id)
+def write_phenopacket2json_file(phenopacket: Phenopacket, out_dr: str) -> None:
+    json_str = _map_phenopacket2json_str(phenopacket)
+    out_path = os.path.join(out_dr, phenopacket.id)
     with open(out_path, 'w') as fh:
         fh.write(json_str)
-        print(f'Successfully wrote phenopacket to JSON {path}')
+        print(f'Successfully wrote phenopacket to JSON {out_dr}')
 
 
-def write_phenopackets2json_file(phenopackets_list: List[Phenopacket], path: str) -> None:
-    # Make sure output path exists
-    os.makedirs(path, exist_ok=True)
+def write_phenopackets2json_files(phenopackets_list: List[Phenopacket], out_dir: str) -> None:
+    # Make sure output out_dr exists
+    os.makedirs(out_dir, exist_ok=True)
 
     for phenopacket in phenopackets_list:
-        write_phenopacket2json_file(phenopacket, path)
+        write_phenopacket2json_file(phenopacket, out_dir)
+
+    print(f'Successfully wrote {len(phenopackets_list)} phenopackets to JSON in {out_dir}')
