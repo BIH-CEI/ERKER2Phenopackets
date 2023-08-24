81094156dfafa0b603a65e63fc0244c6cba1e7f2
Filip Rehburg <fil.rehburg@gmail.com>
2023-08-24 16:16:29 +0200

    refactored mapping of 1 phenopacket into own method

--- MapERKER2Phenopackets.py
+++ MapERKER2Phenopackets.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import pandas as pd
 import phenopackets
 from phenopackets import Individual, PhenotypicFeature, OntologyClass, Phenopacket
@@ -12,11 +14,12 @@ from google.protobuf.internal.well_known_types import Timestamp
 from ParseErker import parse_erker_date_of_birth, parse_erker_sex
 
 
-def create_resource(id: str, name: str, namespace_prefix: str, url: str, version: str, iri_prefix: str) -> Resource:
+def create_resource(phenopacket_id: str, name: str, namespace_prefix: str, url: str, version: str,
+                    iri_prefix: str) -> Resource:
     """A convenience method to create a Phenopacket Schema resource.
 
-    :param id: The id of the resource.
-    :type id: str
+    :param phenopacket_id: The id of the resource.
+    :type phenopacket_id: str
     :param name: The name of the resource.
     :type name: str
     :param namespace_prefix: The namespace prefix of the resource.
@@ -31,88 +34,93 @@ def create_resource(id: str, name: str, namespace_prefix: str, url: str, version
     :rtype: Resource
     """
     return Resource(
-        id=id, name=name, namespace_prefix=namespace_prefix,
+        id=phenopacket_id, name=name, namespace_prefix=namespace_prefix,
         url=url, version=version, iri_prefix=iri_prefix
     )
+
+
+def map_erker_row2phenopacket(phenopacket_id: int, row: pd.Series, resources: List[Resource]):
+    ## Subject
+    # The elements we may consider adding:
+    #  age at last encounter, vital status, karyotypic sex, gender
+
+    subject = Individual(
+        id=str(phenopacket_id),
+        date_of_birth=parse_erker_date_of_birth(row['sct_184099003_y']),
+        sex=parse_erker_sex(row['sct_281053000']),
+        taxonomy=OntologyClass(id='NCBITaxon:9606', label='Homo sapiens')
+    )
+
+    ## PhenotypicFeatures
+    # Unsure if it is useful to add HPO terms in this dataset,
+    #  considering we have the weight measurements.
+    phenotypicFeatures = PhenotypicFeature(
+        type=OntologyClass(id='HP:0001513', label='Obesity')
+
+    )
+    ## Measurements
+    # TODO - the weight course
+    measurements = Measurement(
+
+    )
+
+    ## Interpretations
+    interpretation = Interpretation(
+        id=str(phenopacket_id),
+        progress_status='SOLVED',
+        # diagnosis=Diagnosis(phenopacket_id='ORPHA:71529', label='Obesity due to melanocortin 4 receptor deficiency'),
+    )
+
+    # TODO - add variants
+    variantInterpretation = VariantInterpretation(
+        acmg_pathogenicity_classification='NOT_PROVIDED',  # acmg will be added
+        therapeutic_actionability='UNKNOWN_ACTIONABILITY',
+        #  variant=parse_erker_hgvs()
+
+    )
+
+    ## Disease
+    disease = Disease(
+        term=OntologyClass(id='ORPHA:71529', label='Obesity due to melanocortin 4 receptor deficiency'),
+        #    onset=parse_erker_onset(row['sct_424850005']),
+    )
+
+    ## MetaData
+    created = Timestamp()
+    created.GetCurrentTime()
+
+    meta_data = MetaData(
+        created=created,
+        created_by=created_by,
+        submitted_by=created_by,  # The same for simplicity
+        resources=resources,
+        phenopacket_schema_version=phenopacket_schema_version
+    )
+
+    phenopacket = Phenopacket(
+        id=f'{i}-{phenopacket_id}',  # TODO - can we come up with a better ID?
+        subject=subject,
+        phenotypic_features=[phenotypicFeatures],
+        interpretations=[interpretation],
+        meta_data=meta_data,
+        measurements=[],
+
+    )
+    return phenopacket
 
 
 def map_erker2phenopackets(df: pd.DataFrame, created_by: str, phenopacket_schema_version=phenopackets.__version__):
     erker_phenopackets = []
     # vorher quasi nur mapping, hier zusammensetzen
-    for i, (idx, row) in enumerate(df.iterrows()):
-        ## Subject
-        # The elements we may consider adding:
-        #  age at last encounter, vital status, karyotypic sex, gender
+    resources = [
+        create_resource('ncbitaxon', 'NCBI organismal classification', 'NCBITaxon',
+                        'http://purl.obolibrary.org/obo/ncbitaxon.owl', '2021-06-10',
+                        'http://purl.obolibrary.org/obo/NCBITaxon_')
+    ]
 
-        subject = Individual(
-            id=str(idx),
-            date_of_birth=parse_erker_date_of_birth(row['sct_184099003_y']),
-            sex=parse_erker_sex(row['sct_281053000']),
-            taxonomy=OntologyClass(id='NCBITaxon:9606', label='Homo sapiens')
-        )
-
-        ## PhenotypicFeatures
-        # Unsure if it is useful to add HPO terms in this dataset,
-        #  considering we have the weight measurements.
-        phenotypicFeatures = PhenotypicFeature(
-            type=OntologyClass(id='HP:0001513', label='Obesity')
-
-        )
-        ## Measurements
-        # TODO - the weight course
-        measurements = Measurement(
-
-        )
-
-        ## Interpretations
-        interpretation = Interpretation(
-            id=str(idx),
-            progress_status='SOLVED',
-            # diagnosis=Diagnosis(id='ORPHA:71529', label='Obesity due to melanocortin 4 receptor deficiency'),
-        )
-
-        # TODO - add variants
-        variantInterpretation = VariantInterpretation(
-            acmg_pathogenicity_classification='NOT_PROVIDED',  # acmg will be added
-            therapeutic_actionability='UNKNOWN_ACTIONABILITY',
-            #  variant=parse_erker_hgvs()
-
-        )
-
-        ## Disease
-        disease = Disease(
-            term=OntologyClass(id='ORPHA:71529', label='Obesity due to melanocortin 4 receptor deficiency'),
-            #    onset=parse_erker_onset(row['sct_424850005']),
-        )
-
-        ## MetaData
-        created = Timestamp()
-        created.GetCurrentTime()
-
-        resources = [
-            create_resource('ncbitaxon', 'NCBI organismal classification', 'NCBITaxon',
-                            'http://purl.obolibrary.org/obo/ncbitaxon.owl', '2021-06-10',
-                            'http://purl.obolibrary.org/obo/NCBITaxon_')
-        ]
-
-        meta_data = MetaData(
-            created=created,
-            created_by=created_by,
-            submitted_by=created_by,  # The same for simplicity
-            resources=resources,
-            phenopacket_schema_version=phenopacket_schema_version
-        )
-
-        pp = Phenopacket(
-            id=f'{i}-{idx}',  # TODO - can we come up with a better ID?
-            subject=subject,
-            phenotypic_features=[phenotypicFeatures],
-            interpretations=[interpretation],
-            meta_data=meta_data,
-            measurements=[],
-
-        )
-        erker_phenopackets.append(pp)
+    for i, (phenopacket_id, row) in enumerate(df.iterrows()):
+        phenopacket = map_erker_row2phenopacket(phenopacket_id, row, resources)
+        erker_phenopackets.append(phenopacket)
 
     print(f'Mapped {len(erker_phenopackets)} phenopackets')
     return erker_phenopackets
