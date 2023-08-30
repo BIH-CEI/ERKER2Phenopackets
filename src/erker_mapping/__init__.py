254313cb6217c765ac815abb95093a7da1deb401
Filip Rehburg <fil.rehburg@gmail.com>
2023-08-30 16:34:00 +0200

    renamed write_phenopackets2json_files to write_phenopackets in init, for importing

--- src/erker_mapping/__init__.py
+++ src/erker_mapping/__init__.py
@@ -5,7 +5,7 @@ from .MappingDicts import sex_map_erker2phenopackets, onset_map_erker2phenopacke
 from .ParseErker import parse_erker_agerange, parse_erker_date_of_birth, \
     parse_erker_datediagnosis, parse_erker_hgvs, parse_erker_onset, parse_erker_sex, \
     parse_erker_zygosity
-from .Phenopackets2JSON import write_phenopacket2json_file, \
+from .Phenopackets2JSON import write_phenopacket2json_file as write_phenopackets, \
     write_phenopackets2json_files
 
 __all__ = [
@@ -18,5 +18,5 @@ __all__ = [
     'parse_erker_agerange', 'parse_erker_date_of_birth', 'parse_erker_datediagnosis',
     'parse_erker_hgvs', 'parse_erker_onset', 'parse_erker_sex', 'parse_erker_zygosity',
 
-    'write_phenopackets2json_files', 'write_phenopacket2json_file'
+    'write_phenopackets2json_files', 'write_phenopackets'
 ]
