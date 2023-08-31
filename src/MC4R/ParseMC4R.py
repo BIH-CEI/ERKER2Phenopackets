9e110daea31f635cfd397169d002a99544a2157a
graefea <109136019+graefea@users.noreply.github.com>
2023-08-31 15:19:47 +0100

    29 parse sex (#35)
    * integrated parse_sex function
    
    * integrated doc with examples for function
    
    * created test_parse_sex
    
    * fixed test_parse_sex
    
    * created init.py in MC4R file
    
    * created mapping dicts and copied sex dict in MC4R
    
    * updated import and fixed parse_sex
    
    * deleted import as they were not used
    
    * Update __init__.py to include parse methods
    
    * added parametrized test
    
    * Added pytest to imports
    
    * removed fetus unknown sex from test
    
    * switched OTHER to UNKNOWN sex
    
    ---------
    
    Co-authored-by: frehburg <43171336+frehburg@users.noreply.github.com>

--- src/MC4R/ParseMC4R.py
+++ src/MC4R/ParseMC4R.py
@@ -1,3 +1,4 @@
+from . import sex_map_erker2phenopackets
 # 1. method definition
 # 2. doc (with examples)
 #   a. Title
@@ -7,6 +8,7 @@
 # 3. tests
 # 4. implement
 # 5. activate tests
+
 
 def parse_year_of_birth(year_of_birth: int) -> str:
     """Parses a patient's year of birth to ISO8601 UTC timestamp \
@@ -37,4 +39,33 @@ def parse_year_of_birth(year_of_birth: int) -> str:
         raise ValueError(f'year_of_birth has to be within 1900 and 2023,'
         f'but was {year_of_birth}')
     return f'{year_of_birth}-01-01T00:00:00Z'
+    
+
+
+def parse_sex(sex: str) -> str:
+    """Parses the sex (SNOMED) of a patient entry from ERKER to a Phenopackets sex code.
+
+    :param sex: The sex of the patient.
+    :type sex: str
+    :return: A string code representing the sex of the patient.
+    :raises: Value Error: If the sex string is not a valid SNOMED code
+
+    Could be: 
+    'sct_248152002': 'FEMALE',
+    'sct_248153007': 'MALE',
+    'sct_184115007': 'UNKNOWN_SEX',
+    'sct_33791000087105': 'OTHER_SEX',
+
+    Example:
+    parse_sex(sct_248152002):
+    >>> 'FEMALE'
+
+    Link to Phenopackets documentation, where requirement is defined:
+    https://phenopacket-schema.readthedocs.io/en/latest/sex.html 
+    """
+
+    if sex in sex_map_erker2phenopackets:
+        return sex_map_erker2phenopackets[sex]
+    else:
+        raise ValueError(f'Unknown sex zygosity {sex}')
     
--- tests/test_ParseM4CR.py
+++ tests/test_ParseM4CR.py
@@ -1,7 +1,22 @@
-from MC4R.ParseMC4R import parse_year_of_birth
+from MC4R.ParseMC4R import parse_year_of_birth, parse_sex
+import pytest
 
 def test_parse_year_of_birth():
     example_yob = 2000
     expected_ret = "2000-01-01T00:00:00Z"
 
     assert parse_year_of_birth(example_yob) == expected_ret
+
+
+@pytest.mark.parametrize(
+    ('inp', 'expected'),
+    (
+        ('sct_248153007', 'MALE'),
+        ('sct_248152002', 'FEMALE'),
+        ('sct_33791000087105', 'OTHER_SEX'),
+        ('sct_184115007', 'UNKNOWN_SEX'),
+    )
+)
+def test_parse_sex(inp, expected):
+    
+    assert parse_sex(inp) == expected
