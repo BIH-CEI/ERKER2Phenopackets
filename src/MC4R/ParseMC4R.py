473c149f6c8c5aa84a68f6395b6a3c49ba7aef76
Adam Graefe <adam.graefe@charite.de>
2023-08-31 13:57:41 +0100

    updated import and fixed parse_sex

--- src/MC4R/ParseMC4R.py
+++ src/MC4R/ParseMC4R.py
@@ -1,3 +1,4 @@
+from . import sex_map_erker2phenopackets
 # 1. method definition
 # 2. doc (with examples)
 #   a. Title
@@ -7,8 +8,6 @@
 # 3. tests
 # 4. implement
 # 5. activate tests
-
-from old_erker_mapping import MapERKER2Phenopackets
 
 
 def parse_year_of_birth(year_of_birth: int) -> str:
@@ -65,8 +64,8 @@ def parse_sex(sex: str) -> str:
     https://phenopacket-schema.readthedocs.io/en/latest/sex.html 
     """
 
-    if sex in MapERKER2Phenopackets.sex_map_erker2phenopackets:
-        return MapERKER2Phenopackets.sex_map_erker2phenopackets[sex]
+    if sex in sex_map_erker2phenopackets:
+        return sex_map_erker2phenopackets[sex]
     else:
         raise ValueError(f'Unknown sex zygosity {sex}')
     
