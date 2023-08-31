20fc1ee673837c37ac63fdcb4c02933f4f454e3f
graefea <109136019+graefea@users.noreply.github.com>
2023-08-31 11:26:14 +0100

    updated mappingdicts (#23)

--- src/old_erker_mapping/MappingDicts.py
+++ src/old_erker_mapping/MappingDicts.py
@@ -1,17 +1,16 @@
+sex_map_erker2phenopackets = {
+    'sct_248152002': 'FEMALE',
+    'sct_248153007': 'MALE',
+    'sct_184115007': 'UNKNOWN_SEX',  # Unbestimmt
+    'sct_33791000087105': 'OTHER_SEX',  # Divers
+}
+
+
 zygosity_map_erker2phenopackets = {
-    'sct_22061001': 'homozygous',
-    'sct_14556007': 'heterozygous'
+    'ln_LA6705-3': 'GENO:0000136', # homozygous
+    'ln_LA6706-1': 'GENO:0000135' # heterozygous
 }
 
-date_diagnosis_map_erker2phenopackets = {
-    'sct_424850005': 'Date_diagnois',
-}
-
-onset_map_erker2phenopackets = {
-    'sct_118189007': 'HP:0030674',  # Antenatal onset
-    'sct_364586004': 'HP:0003577',  # Congenital onset
-    'sct_424850005': 'HP:0003674',  # onset_date #onset
-}
 
 age_range_map_erker2phenopackets = {
     'sct_133931009': ('P0Y', 'P1Y'),
@@ -19,13 +18,4 @@ age_range_map_erker2phenopackets = {
     'sct_410600008': ('P6Y', 'P12Y'),
     'sct_133937008': ('P12Y', 'P18Y'),
     'sct_13393600': ('P18Y', 'P99Y')
-}
-
-sex_map_erker2phenopackets = {
-    'sct_248152002': 'FEMALE',
-    'sct_248153007': 'MALE',
-
-    'sct_184115007': 'OTHER_SEX',  # Unbestimmt
-    'sct_33791000087105': 'OTHER_SEX',  # Divers
-    'sct_394743007_foetus': 'UNKNOWN_SEX'  # Fötus (unbekannt)
 }
