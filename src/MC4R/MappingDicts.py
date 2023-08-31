bb9ba890acc19c3556aae6b16cd3432c39cd8ecf
Adam Graefe <adam.graefe@charite.de>
2023-08-31 13:57:28 +0100

    created mapping dicts and copied sex dict in MC4R

---
+++ src/MC4R/MappingDicts.py
@@ -0,0 +1,6 @@
+sex_map_erker2phenopackets = {
+    'sct_248152002': 'FEMALE',
+    'sct_248153007': 'MALE',
+    'sct_184115007': 'UNKNOWN_SEX',  # Unbestimmt
+    'sct_33791000087105': 'OTHER_SEX',  # Divers
+}
