8f17aa3d06cb3642055a2766eb45ebd33b8a0ab4
Filip Rehburg <fil.rehburg@gmail.com>
2023-08-31 14:35:56 +0200

    Added utils to remove null columns and count rows/ cols

---
+++ src/PolarsUtils.py
@@ -0,0 +1,53 @@
+from typing import List
+
+import polars as pl
+
+
+def remove_all_null_cols(df: pl.DataFrame) -> pl.DataFrame:
+    """
+    Remove all columns that have only null values
+    :param df: DataFrame with columns with only null values
+    :type df: pl.DataFrame
+    :return:  without only null columns
+    :rtype: pl.DataFrame
+    """
+    all_null_cols = get_all_null_cols(df)
+    return df.drop(all_null_cols)
+
+
+def get_all_null_cols(df: pl.DataFrame) -> List[str]:
+    """
+    Get names of all columns that have only null values
+    :param df: DataFrame with columns with only null values
+    :type df: pl.DataFrame
+    :return: List of column names with only null values
+    :rtype: List[str]
+    """
+    return df.select(
+        pl.all()
+        .is_null()
+        .all()
+    ).melt().filter(
+        pl.col('value')  # == True
+    ).select('variable').to_series().to_list()
+
+
+def get_num_cols(df: pl.DataFrame) -> int:
+    """
+    Get number of columns in DataFrame
+    :param df: DataFrame
+    :type df: pl.DataFrame
+    :return: number of columns
+    :rtype: int
+    """
+    return df.shape[1]
+
+
+def get_num_rows(df: pl.DataFrame) -> int:
+    """
+    Get number of rows in DataFrame
+    :param df: DataFrame
+    :return: number of rows
+    :rtype: int
+    """
+    return df.shape[0]
