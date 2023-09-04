842a08e40a63901f3fcf475d1497378f272b8f4c
graefea <109136019+graefea@users.noreply.github.com>
2023-09-04 13:52:15 +0100

    31 parse date of diagnosis (#39)
    * added parse dat of diagnosis and description
    
    * added if function to parse date of diagnosis
    
    * added test_parse_date_of_diagnosis
    
    * changed month to expexted input string
    
    * fixed "month" input
    
    * added data types for year month and day
    
    * stated which which one is target format
    
    * updated definition of functoion
    
    * updated old variables in function definition
    
    * Update src/MC4R/ParseMC4R.py
    
    Co-authored-by: frehburg <43171336+frehburg@users.noreply.github.com>
    
    * Update src/MC4R/ParseMC4R.py
    
    Co-authored-by: frehburg <43171336+frehburg@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: frehburg <43171336+frehburg@users.noreply.github.com>

--- tests/test_ParseM4CR.py
+++ tests/test_ParseM4CR.py
@@ -8,6 +8,15 @@ def test_parse_year_of_birth():
     assert parse_year_of_birth(example_yob) == expected_ret
 
 
+def test_parse_date_of_diagnosis(): 
+    example_y = 2004
+    example_m = "04"
+    example_d = 21
+    expected_ret = "2004-04-21T00:00:00Z"
+
+    assert test_parse_date_of_diagnosis(example_y, example_m, example_d) == expected_ret
+
+    
 @pytest.mark.parametrize(
     ('inp', 'expected'),
     (
