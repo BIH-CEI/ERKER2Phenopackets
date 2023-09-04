b7774cc80adc8ac1a9fba91b07212dee3a1a7de5
Filip Rehburg <fil.rehburg@gmail.com>
2023-09-04 16:16:54 +0200

    reformatted and added ,

--- tests/test_ParseM4CR.py
+++ tests/test_ParseM4CR.py
@@ -2,6 +2,7 @@ from src.MC4R.ParseMC4R import parse_year_of_birth, parse_sex, parse_zygosity
 
 import pytest
 
+
 def test_parse_year_of_birth():
     example_yob = 2000
     expected_ret = "2000-01-01T00:00:00Z"
@@ -12,14 +13,13 @@ def test_parse_year_of_birth():
 @pytest.mark.parametrize(
     ('inp', 'expected'),
     (
-        ('sct_248153007', 'MALE'),
-        ('sct_248152002', 'FEMALE'),
-        ('sct_33791000087105', 'OTHER_SEX'),
-        ('sct_184115007', 'UNKNOWN_SEX'),
+            ('sct_248153007', 'MALE'),
+            ('sct_248152002', 'FEMALE'),
+            ('sct_33791000087105', 'OTHER_SEX'),
+            ('sct_184115007', 'UNKNOWN_SEX'),
     )
 )
 def test_parse_sex(inp, expected):
-    
     assert parse_sex(inp) == expected
 
 
@@ -32,6 +32,5 @@ def test_parse_sex(inp, expected):
 
     )
 )
-
 def test_parse_zygosity(inp, expected):
     assert parse_zygosity(inp) == expected
