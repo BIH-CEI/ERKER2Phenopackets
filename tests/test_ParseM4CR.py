8a047bc6184c8510909cb51280505bcf3f1e29ba
Adam Graefe <adam.graefe@charite.de>
2023-09-04 10:21:19 +0100

    added test_parse_date_of_diagnosis

--- tests/test_ParseM4CR.py
+++ tests/test_ParseM4CR.py
@@ -5,3 +5,13 @@ def test_parse_year_of_birth():
     expected_ret = "2000-01-01T00:00:00Z"
 
     assert parse_year_of_birth(example_yob) == expected_ret
+
+
+
+def test_parse_date_of_diagnosis(): 
+    example_y = 2004
+    example_m = 4
+    example_d = 21
+    expected_ret = "2004-04-21T00:00:00Z"
+
+    assert test_parse_date_of_diagnosis(example_y, example_m, example_d) == expected_ret
