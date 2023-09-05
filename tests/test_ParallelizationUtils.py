ab889229083b9a998fc47153ef548b6392c46dfb
Filip Rehburg <fil.rehburg@gmail.com>
2023-09-05 14:50:40 +0200

    added test for split_dataframe

--- tests/test_ParallelizationUtils.py
+++ tests/test_ParallelizationUtils.py
@@ -1,6 +1,7 @@
 import pytest
+import polars as pl
 
-from src.utils.ParallelizationUtils import calc_chunk_size
+from src.utils.ParallelizationUtils import calc_chunk_size, split_dataframe
 
 
 @pytest.mark.parametrize(
@@ -28,3 +29,35 @@ def test_calc_chunk_size(num_instances, num_chunks, expected):
 def test_calc_chunk_size_invalid_params(num_instances, num_chunks, expected):
     with pytest.raises(ValueError):
         calc_chunk_size(num_instances, num_chunks)
+
+
+@pytest.mark.parametrize(
+    ('df', 'num_chunks'),
+    (
+            (
+                pl.DataFrame(
+                    {
+                        "a": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                        "b": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                        "c": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                    }
+                ),
+                3
+            ),
+            (
+                pl.DataFrame(
+                    {
+                        "a": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+                        "b": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+                        "c": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+                    }
+                ),
+                4
+            ),
+    )
+)
+def test_split_dataframe(df, num_chunks):
+    chunk_sizes = calc_chunk_size(num_instances=df.height, num_chunks=num_chunks)
+
+    for i, df_sub in enumerate(split_dataframe(df, chunk_sizes)):
+        assert df_sub.height == chunk_sizes[i]
