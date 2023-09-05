05d9eba8311ef397e1906dd3f63a0645c665d4a2
Filip Rehburg <fil.rehburg@gmail.com>
2023-09-05 14:00:52 +0200

    created init in utils

---
+++ src/utils/__init__.py
@@ -0,0 +1,4 @@
+from .Phenopackets2JSON import write_phenopackets2json_files as write_files
+from .Phenopackets2JSON import write_phenopacket2json_file as write_file
+
+__all__ = ['write_file', 'write_files']
