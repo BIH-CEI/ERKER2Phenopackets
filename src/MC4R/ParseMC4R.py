f2738da1851369c3a194669d7937663a546816d0
Adam Graefe <adam.graefe@charite.de>
2023-09-05 16:53:12 +0100

    deleted unused import

--- src/MC4R/ParseMC4R.py
+++ src/MC4R/ParseMC4R.py
@@ -1,5 +1,3 @@
-from typing import Union
-
 from . import sex_map_erker2phenopackets, zygosity_map_erker2phenopackets
 from ..utils.ParsingUtils import parse_date_string_to_iso8601_utc_timestamp, \
     parse_year_month_day_to_iso8601_utc_timestamp
