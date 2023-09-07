fa624826c367b55022a23af351b4863b5df2ae48
Rehburg, Filip (Stud. DACS) <f.rehburg@student.maastrichtuniversity.nl>
2023-09-07 17:27:05 +0200

    added dict to map phenotype label

--- src/MC4R/MappingDicts.py
+++ src/MC4R/MappingDicts.py
@@ -6,8 +6,15 @@ sex_map_erker2phenopackets = {
 }
 
 zygosity_map_erker2phenopackets = {
-    'ln_LA6705-3': 'GENO:0000136', # homozygous
-    'ln_LA6706-1': 'GENO:0000135', # heterozygous
-    'ln_LA6707-9': 'GENO:0000134', # hemizygous
-    'sct_1220561009': 'GENO:0000137', # unspecified zygosity
+    'ln_LA6705-3': 'GENO:0000136',  # homozygous
+    'ln_LA6706-1': 'GENO:0000135',  # heterozygous
+    'ln_LA6707-9': 'GENO:0000134',  # hemizygous
+    'sct_1220561009': 'GENO:0000137',  # unspecified zygosity
+}
+
+phenotype_label_map_erker2phenopackets = {
+    "HP:0025501": 'Class III obesity',
+    "HP:0025500": 'Class II obesity',
+    "HP:0025499": 'Class I obesity',
+    "HP:0025502": 'Overweight',
 }
