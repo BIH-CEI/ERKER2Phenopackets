f5b92e9dd84c3978f83cc960ff2ff6cc4e1f3efc
Filip Rehburg <fil.rehburg@gmail.com>
2023-09-11 14:12:26 +0200

    removed unnecessary files

--- src/logging_/__init__.py
+++
@@ -1,5 +0,0 @@
-from .Logging_ import LOG_LEVELS, setup_logging
-
-__all__ = [
-    "LOG_LEVELS", "setup_logging",
-]
--- tests/Foo_test.py
+++
@@ -1,5 +0,0 @@
-from src.Foo import foo
-
-
-def test_foo():
-    assert foo(1) == 2
