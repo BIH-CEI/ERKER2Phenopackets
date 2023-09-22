729cf46852537dea434921833aa74df0e260ae54
Adam Graefe <adam.graefe@charite.de>
2023-09-22 09:44:03 +0100

    added deprecation decorator

---
+++ ERKER2Phenopackets/src/decorators/deprecate.py
@@ -0,0 +1,16 @@
+import warnings
+import functools
+from loguru import logger
+
+
+def deprecated(reason):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            logger.warning(f'DeprecationWarning: {func.__name__} is deprecated.' 
+                           f'{reason}')
+            warnings.warn(f'{func.__name__} is deprecated. {reason}', 
+                          category=DeprecationWarning)
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
