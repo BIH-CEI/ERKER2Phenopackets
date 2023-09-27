3bc84a429d975911d988fb34caa6f1a1cdc00e93
Filip Rehburg <fil.rehburg@gmail.com>
2023-09-27 20:07:54 +0200

    added delete files in folder script

---
+++ ERKER2Phenopackets/src/utils/delete_files_in_folder.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from typing import Union, List
+from loguru import logger
+
+
+def delete_files_in_folder(dirs: Union[Path, List[Path]], file_extension: str = None):
+    """Deletes all files in a folder
+
+    This function deletes all files in a folder. If a file extension is given,
+    only files with that file extension are deleted.
+
+    :param dirs: Path to the folder or a list of paths to folders
+    :type dirs: Union[Path, List[Path]]
+    :param file_extension: File extension of the files that should be deleted
+    :type file_extension: str
+    """
+    if isinstance(dirs, Path):
+        dirs = [dirs]
+
+    if file_extension:
+        logger.info(f"Starting the delete_files_in_folder method with "
+                    f"file extension {file_extension}")
+    else:
+        logger.info("Starting the delete_files_in_folder method")
+
+    for directory in dirs:
+        logger.info(f"Checking the directory {directory}")
+        for file in directory.iterdir():
+            if file.is_file():
+                if file_extension and file_extension in file.suffix:
+                    logger.trace(f"Deleting the file {file}")
+                    file.unlink()
+
+    logger.info("Finished the delete_files_in_folder method")
