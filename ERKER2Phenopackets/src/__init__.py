74a5abe9cb623df6fa185219251a8d4ccc8957eb
Filip Rehburg <fil.rehburg@gmail.com>
2023-10-02 11:22:34 +0200

    added `__init__.py` for src

---
+++ ERKER2Phenopackets/src/__init__.py
@@ -0,0 +1,13 @@
+from . import MC4R
+from .MC4R.Pipeline import pipeline
+
+from . import utils
+from .utils import validate
+
+__all__ = [
+    'MC4R',
+    'pipeline',
+
+    'utils',
+    'validate',
+]
