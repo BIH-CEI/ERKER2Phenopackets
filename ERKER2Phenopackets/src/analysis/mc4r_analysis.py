709e460a83a98a03d63c06b3f0f2457891698352
Filip Rehburg <fil.rehburg@gmail.com>
2023-10-03 15:24:30 +0200

    added skeleton for analysis

---
+++ ERKER2Phenopackets/src/analysis/mc4r_analysis.py
@@ -0,0 +1,16 @@
+def analyse():
+    """Analyse Phenopackets concerning the MC4R gene and its variants.
+
+    TODO @grafea: Please write a short description of what exactly we will analyse
+    TODO @grafea: here and why
+    """
+    pass
+
+
+def main():
+    analyse()
+
+
+if __name__ == "__main__":
+    main()
+    
