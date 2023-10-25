451697b4fc8a05ad03d89fde872559f28c0681d5
Filip Rehburg <fil.rehburg@gmail.com>
2023-10-25 19:02:47 +0200

    implemented compare structure

--- ERKER2Phenopackets/src/analysis/comparison/edit_distance.py
+++ ERKER2Phenopackets/src/analysis/comparison/edit_distance.py
@@ -1,6 +1,8 @@
 import uuid
 from typing import Dict, Tuple, Union, Optional
 
+from .traverse import traverse
+
 
 def compare_structure(
         d1: Dict, d2: Dict,
@@ -8,6 +10,10 @@ def compare_structure(
         d2_id: Optional[Union[int, str]] = uuid.uuid4()
 ) -> Tuple[bool, Dict]:
     """Compares if the structure of two dictionaries match.
+
+    By structure we mean the keys and the order of the keys.
+
+    TODO: WARNING currently does not return a difference tree, but only a boolean
 
     :param d1: First dictionary
     :type d1: Dict
@@ -19,3 +25,16 @@ def compare_structure(
     :type d2_id: Optional[Union[int, str]], optional
     :return: True if structure matches, False and difference dict otherwise
     """
+    bfs1 = traverse(d1, 'bfs', include_vals=False)
+    bfs2 = traverse(d2, 'bfs', include_vals=False)
+
+    if not bfs1 == bfs2:
+        return False, create_difference_tree(d1, d2, d1_id, d2_id)
+
+    dfs1 = traverse(d1, 'dfs', include_vals=False)
+    dfs2 = traverse(d2, 'dfs', include_vals=False)
+
+    if not dfs1 == dfs2:
+        return False, create_difference_tree(d1, d2, d1_id, d2_id)
+
+    return True, {}
