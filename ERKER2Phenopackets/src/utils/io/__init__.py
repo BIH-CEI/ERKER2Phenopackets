f307ed851e1beb6f2d000a30823ad7ae249a32c8
Filip Rehburg <fil.rehburg@gmail.com>
2023-10-25 18:51:18 +0200

    added function to create dict from phenopacket

--- ERKER2Phenopackets/src/utils/io/__init__.py
+++ ERKER2Phenopackets/src/utils/io/__init__.py
@@ -4,8 +4,12 @@ from .phenopackets2json import write_phenopackets2json_files as write_files, \
 from .json2phenopackets import read_json_files2phenopackets as read_files, \
     read_json_file2phenopacket as read_file
 
+from .phenopackets2dict import phenopacket2dict
+
 __all__ = [
     'write_file', 'write_files',
 
     'read_file', 'read_files',
+
+    'phenopacket2dict',
 ]
