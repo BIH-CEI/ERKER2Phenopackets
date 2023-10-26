5975601c04eb5e331b399c566f66b98bf6d300b7
Filip Rehburg <fil.rehburg@gmail.com>
2023-10-26 14:12:35 +0200

    wrote test case for compare structure

--- ERKER2Phenopackets/tests/test_structure.py
+++ ERKER2Phenopackets/tests/test_structure.py
@@ -1,5 +1,5 @@
 from ERKER2Phenopackets.src.analysis.comparison.structure import assign_dict_at, \
-    create_difference_tree
+    create_difference_tree, compare_structure
 
 
 def test_assign_dict_at():
@@ -33,4 +33,26 @@ def test_create_difference_tree():
     d5 = {'a': {'b': {'c': [3]}}}
     diff = create_difference_tree(d4, d5, 4, 5)
     expected = {'a': {'b': {'c': {4: [2], 5: [3]}}}}
+    assert diff == expected
+
+
+def test_compare_structure():
+    d1 = {'a': {'b': {'c': 2}}}
+    d2 = {'a': {'b': {'c': 3}}}
+    equals, diff = compare_structure(d1, d2, 1, 2)
+    expected = {}
+    assert equals
+    assert diff == expected
+
+    d3 = {'a': {'b': {'d': 2}}}
+    equals, diff = compare_structure(d1, d3, 1, 3)
+    expected = {'a': {'b': {1: {'c': 2}, 3: {'d': 2}}}}
+    assert not equals
+    assert diff == expected
+
+    d4 = {'a': {'b': {'c': [2]}}}
+    d5 = {'a': {'b': {'c': [3]}}}
+    equals, diff = compare_structure(d4, d5, 4, 5)
+    expected = {'a': {'b': {'c': {4: [2], 5: [3]}}}}
+    assert equals
     assert diff == expected
