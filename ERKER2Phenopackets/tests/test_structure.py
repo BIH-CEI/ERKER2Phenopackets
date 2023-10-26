5133d2b9052e6d38e85fc144d839aa9af14b2560
Filip Rehburg <fil.rehburg@gmail.com>
2023-10-26 13:17:52 +0200

    added test for difference tree

--- ERKER2Phenopackets/tests/test_structure.py
+++ ERKER2Phenopackets/tests/test_structure.py
@@ -1,4 +1,5 @@
-from ERKER2Phenopackets.src.analysis.comparison.structure import assign_dict_at
+from ERKER2Phenopackets.src.analysis.comparison.structure import assign_dict_at, \
+    create_difference_tree
 
 
 def test_assign_dict_at():
@@ -14,3 +15,22 @@ def test_assign_dict_at():
         key_path=['a', 'b', 'c', 0],
         value=3
     ) == {'a': {'b': {'c': [3]}}})
+
+
+def test_create_difference_tree():
+    d1 = {'a': {'b': {'c': 2}}}
+    d2 = {'a': {'b': {'c': 3}}}
+    diff = create_difference_tree(d1, d2, 1, 2)
+    expected = {'a': {'b': {'c': {}}}}
+    assert diff == expected
+
+    d3 = {'a': {'b': {'d': 2}}}
+    diff = create_difference_tree(d1, d3, 1, 3)
+    expected = {'a': {'b': {1: {'c': {}}, 3: {'d': {}}}}}
+    assert diff == expected
+
+    d4 = {'a': {'b': {'c': [2]}}}
+    d5 = {'a': {'b': {'c': [3]}}}
+    diff = create_difference_tree(d4, d5, 4, 5)
+    expected = {'a': {'b': {'c': {4: [2], 5: [3]}}}}
+    assert diff == expected
