ae2c134ba8fba0c11622627d6de0c0e45047f380
Filip Rehburg <fil.rehburg@gmail.com>
2023-10-26 14:25:58 +0200

    updated traversal to include 'list' as if it was a key

--- ERKER2Phenopackets/src/analysis/comparison/traverse.py
+++ ERKER2Phenopackets/src/analysis/comparison/traverse.py
@@ -37,6 +37,7 @@ def bfs(d: Dict, include_vals: bool = True) -> List:
                 traversal.append(key)
                 queue.append(value)
         elif isinstance(node, list) or isinstance(node, tuple):
+            traversal.append('list')
             for value in node:
                 queue.append(value)
         elif include_vals:
@@ -59,6 +60,7 @@ def dfs(d: Dict, include_vals: bool = True) -> List:
                 stack.append(value)
                 traversal.append(key)
         elif isinstance(node, list) or isinstance(node, tuple):
+            traversal.append('list')
             for value in reversed(node):
                 stack.append(value)
         elif include_vals:
