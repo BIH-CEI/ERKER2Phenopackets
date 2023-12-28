23e71ea140842bf1336c5b5a35c88220cacc4675
Adam Graefe <adam.graefe@charite.de>
2023-12-28 17:43:19 +0100

    shortening

--- ERKER2Phenopackets/src/mc4r/pipeline.py
+++ ERKER2Phenopackets/src/mc4r/pipeline.py
@@ -292,7 +292,6 @@ def pipeline(
                                 map_to='parsed_date_of_phenotyping6',
                                 mapping=parse_phenotyping_date)
         df = polars_utils.fill_null_vals(df, 'parsed_date_of_phenotyping6', no_date)
-
     if 'sct_8116006_7_date' in df.columns:
         df = polars_utils.map_col(df, map_from='sct_8116006_7_date',
                                 map_to='parsed_date_of_phenotyping7',
@@ -304,19 +303,16 @@ def pipeline(
                                 map_to='parsed_date_of_phenotyping8',
                                 mapping=parse_phenotyping_date)
         df = polars_utils.fill_null_vals(df, 'parsed_date_of_phenotyping8', no_date)
-
     if 'sct_8116006_9_date' in df.columns:
         df = polars_utils.map_col(df, map_from='sct_8116006_9_date',
                                 map_to='parsed_date_of_phenotyping9',
                                 mapping=parse_phenotyping_date)
         df = polars_utils.fill_null_vals(df, 'parsed_date_of_phenotyping9', no_date)
-        
     if 'sct_8116006_10_date' in df.columns:
         df = polars_utils.map_col(df, map_from='sct_8116006_10_date',
                                 map_to='parsed_date_of_phenotyping10',
                                 mapping=parse_phenotyping_date)
         df = polars_utils.fill_null_vals(df, 'parsed_date_of_phenotyping10', no_date)
-
     if 'sct_8116006_11_date' in df.columns:
         df = polars_utils.map_col(df, map_from='sct_8116006_11_date',
                                 map_to='parsed_date_of_phenotyping11',
